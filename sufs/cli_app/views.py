16c7f862a759a2d89e2bd274490e1f3d04ae15b8
Tom Gleitsmann <35902221+tmgleitsmann@users.noreply.github.com>
2019-03-01 15:43:34 -0800

    added extra function to interact w/ client.

--- sufs/cli_app/views.py
+++ sufs/cli_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 def cli_home(request):
     # cli_message = "hello from the client!"
@@ -18,4 +17,9 @@ def post(request):
 	#define what happens during route call.
 	return HttpResponse('<h1>Cli POST<h1>')
 
+def post_NN(request, size, data1, NNIPAdress):
+	#we need a post request from here to the NN.
+	
+	#we want to await a response before we return a value to client.py
+	return 'sufs.cli_app.views.post_NN() was called upon'
 # Create your views here.
